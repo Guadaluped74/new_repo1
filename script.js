6586cf24fe5e28c292992628bad6d4de86483f56
Aidee De Arcos Ramirez <gbdearcos19@gmail.com>
2021-04-05 18:16:30 -0700

    changes

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function generatePassword () {
 var character = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
 
   var capital = confirm("Capital letters?");
-  if (capital === true) {character.push(capLetter);
+  if (capital === true) {character.push(upperLetter);
   alert("Capital letters selected");
   } 
   else alert("No capital letters.");
